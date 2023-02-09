871fed2024a543119f73676ec2b739ea808335f9
igkim <jel4399@gmail.com>
2023-02-09 14:32:15 +0900

    Forked kafka-ui

---
+++ kafka-ui-e2e-checks/README.md
@@ -0,0 +1,100 @@
+### E2E UI automation for Kafka-ui
+
+This repository is for E2E UI automation.
+
+### Table of Contents
+
+- [Prerequisites](#prerequisites)
+- [How to install](#how-to-install)
+- [How to run checks](#how-to-run-checks)
+- [Reporting](#reporting)
+- [Environments setup](#environments-setup)
+- [Test Data](#test-data)
+- [Actions](#actions)
+- [Checks](#checks)
+- [Parallelization](#parallelization)
+- [How to develop](#how-to-develop)
+
+### Prerequisites
+
+- Docker & Docker-compose
+- Java (install aarch64 jdk if you have M1/arm chip)
+- Maven
+
+### How to install
+
+```
+git clone https://github.com/provectus/kafka-ui.git
+cd  kafka-ui-e2e-checks
+docker pull selenoid/vnc:chrome_86.0  
+```
+
+### How to run checks
+
+1. Run `kafka-ui`:
+
+```
+cd kafka-ui
+docker-compose -f documentation/compose/e2e-tests.yaml up -d
+```
+
+2. Run Smoke test suite using your QaseIO API token as environment variable (put instead %s into command below)
+
+```
+./mvnw -DQASEIO_API_TOKEN='%s' -pl 'kafka-ui-e2e-checks' test -Dsurefire.suiteXmlFiles='src/test/resources/smoke.xml' -Pprod
+```
+
+3. Run Regression test suite using your QaseIO API token as environment variable (put instead %s into command below)
+
+```
+./mvnw -DQASEIO_API_TOKEN='%s' -pl 'kafka-ui-e2e-checks' test -Dsurefire.suiteXmlFiles='src/test/resources/regression.xml' -Pprod
+```
+
+4. To run tests on your local Chrome browser just add next VM option to the Run Configuration
+
+```
+-Dbrowser=local
+```
+
+### Reporting
+
+Reports are in `allure-results` folder.
+If you have installed allure commandline [here](https://www.npmjs.com/package/allure-commandline))
+You can see allure report with command:
+
+```
+allure serve
+```
+
+### Screenshots
+
+Reference screenshots are in `SCREENSHOTS_FOLDER`  (default,`kafka-ui-e2e-checks/screenshots`)
+
+### How to develop
+
+> ⚠️ todo
+
+### Setting for different environments
+
+> ⚠️ todo
+
+### Test Data
+
+> ⚠️ todo
+
+### Actions
+
+> ⚠️ todo
+
+### Checks
+
+> ⚠️ todo
+
+### Parallelization
+
+> ⚠️ todo
+
+### Tips
+
+- install `Selenium UI Testing plugin` in IDEA
+
---
+++ kafka-ui-react-app/src/components/Connect/Details/Actions/Actions.tsx
@@ -0,0 +1,149 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+import { useIsMutating } from '@tanstack/react-query';
+import {
+  Action,
+  ConnectorAction,
+  ConnectorState,
+  ResourceType,
+} from 'generated-sources';
+import useAppParams from 'lib/hooks/useAppParams';
+import {
+  useConnector,
+  useDeleteConnector,
+  useUpdateConnectorState,
+} from 'lib/hooks/api/kafkaConnect';
+import {
+  clusterConnectorsPath,
+  RouterParamsClusterConnectConnector,
+} from 'lib/paths';
+import { useConfirm } from 'lib/hooks/useConfirm';
+import { Dropdown } from 'components/common/Dropdown';
+import { ActionDropdownItem } from 'components/common/ActionComponent';
+
+const ConnectorActionsWrapperStyled = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 8px;
+`;
+
+const Actions: React.FC = () => {
+  const navigate = useNavigate();
+  const routerProps = useAppParams<RouterParamsClusterConnectConnector>();
+  const mutationsNumber = useIsMutating();
+  const isMutating = mutationsNumber > 0;
+
+  const { data: connector } = useConnector(routerProps);
+  const confirm = useConfirm();
+
+  const deleteConnectorMutation = useDeleteConnector(routerProps);
+  const deleteConnectorHandler = () =>
+    confirm(
+      <>
+        Are you sure you want to remove <b>{routerProps.connectorName}</b>{' '}
+        connector?
+      </>,
+      async () => {
+        try {
+          await deleteConnectorMutation.mutateAsync();
+          navigate(clusterConnectorsPath(routerProps.clusterName));
+        } catch {
+          // do not redirect
+        }
+      }
+    );
+
+  const stateMutation = useUpdateConnectorState(routerProps);
+  const restartConnectorHandler = () =>
+    stateMutation.mutateAsync(ConnectorAction.RESTART);
+  const restartAllTasksHandler = () =>
+    stateMutation.mutateAsync(ConnectorAction.RESTART_ALL_TASKS);
+  const restartFailedTasksHandler = () =>
+    stateMutation.mutateAsync(ConnectorAction.RESTART_FAILED_TASKS);
+  const pauseConnectorHandler = () =>
+    stateMutation.mutateAsync(ConnectorAction.PAUSE);
+  const resumeConnectorHandler = () =>
+    stateMutation.mutateAsync(ConnectorAction.RESUME);
+
+  return (
+    <ConnectorActionsWrapperStyled>
+      <Dropdown>
+        {connector?.status.state === ConnectorState.RUNNING && (
+          <ActionDropdownItem
+            onClick={pauseConnectorHandler}
+            disabled={isMutating}
+            permission={{
+              resource: ResourceType.CONNECT,
+              action: Action.EDIT,
+              value: routerProps.connectorName,
+            }}
+          >
+            Pause
+          </ActionDropdownItem>
+        )}
+        {connector?.status.state === ConnectorState.PAUSED && (
+          <ActionDropdownItem
+            onClick={resumeConnectorHandler}
+            disabled={isMutating}
+            permission={{
+              resource: ResourceType.CONNECT,
+              action: Action.EDIT,
+              value: routerProps.connectorName,
+            }}
+          >
+            Resume
+          </ActionDropdownItem>
+        )}
+        <ActionDropdownItem
+          onClick={restartConnectorHandler}
+          disabled={isMutating}
+          permission={{
+            resource: ResourceType.CONNECT,
+            action: Action.EDIT,
+            value: routerProps.connectorName,
+          }}
+        >
+          Restart Connector
+        </ActionDropdownItem>
+        <ActionDropdownItem
+          onClick={restartAllTasksHandler}
+          disabled={isMutating}
+          permission={{
+            resource: ResourceType.CONNECT,
+            action: Action.EDIT,
+            value: routerProps.connectorName,
+          }}
+        >
+          Restart All Tasks
+        </ActionDropdownItem>
+        <ActionDropdownItem
+          onClick={restartFailedTasksHandler}
+          disabled={isMutating}
+          permission={{
+            resource: ResourceType.CONNECT,
+            action: Action.EDIT,
+            value: routerProps.connectorName,
+          }}
+        >
+          Restart Failed Tasks
+        </ActionDropdownItem>
+        <ActionDropdownItem
+          onClick={deleteConnectorHandler}
+          disabled={isMutating}
+          danger
+          permission={{
+            resource: ResourceType.CONNECT,
+            action: Action.DELETE,
+            value: routerProps.connectorName,
+          }}
+        >
+          Delete
+        </ActionDropdownItem>
+      </Dropdown>
+    </ConnectorActionsWrapperStyled>
+  );
+};
+
+export default Actions;
